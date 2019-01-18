883234004eea8213fca44fed87e8f93967076c4d
Bashorun Olajide John <bashorunolajide@gmail.com>
2019-01-18 13:20:57 +0100

    [Fixed] No implicit conversion of nil into String

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,16 +1,17 @@
 class JsonWebToken
 
+ HMAC_SECRET = Rails.application.credentials.secret_key_base
  class << self
  	#secret to encode and decode token
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
      
      #Testing Rails.application.secrets.secret_key_base
-     JWT.encode(payload, Rails.application.secrets.secret_key_base)
+     JWT.encode(payload, HMAC_SECRET)
    end
 
    def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+     body = JWT.decode(token, HMAC_SECRET)[0]
      HashWithIndifferentAccess.new body
    rescue
   
